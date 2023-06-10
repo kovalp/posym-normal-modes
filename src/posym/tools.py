51be99fae3f5cd2f7c6f9bc6c3e51a9ab46d0acd
Peter Koval <koval.peter@gmail.com>
2023-06-10 14:23:02 +0200

    Works without C extensions.

---
+++ src/posym/tools.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+
+def list_round(elements_list, decimals=2):
+    r_list = []
+    for element in elements_list:
+        if abs(np.round(element) - element) < 10**(-decimals):
+            r_list.append(int(np.round(element)))
+        else:
+            r_list.append(np.round(element, decimals))
+
+    return r_list
+
+
+def standardize_vector(vector, prec=1e-5):
+
+    vector = np.array(vector, dtype=float)
+    if np.abs(vector[0]) > prec:
+        if vector[0] < 0:
+            vector = np.array(vector) * -1
+    elif np.abs(vector[1]) > prec:
+        if vector[1] < 0:
+            vector = np.array(vector) * -1
+    else:
+        if vector[2] < 0:
+            vector = np.array(vector) * -1
+
+    return vector.tolist()
+
+
+
+
---
+++ test/test_normal_modes.py
@@ -0,0 +1,20 @@
+from posym import SymmetryModes
+
+
+def test_normal_modes() -> None:
+    """."""
+    coordinates = [[0.00000, 0.0000000, -0.0808819],
+                   [-1.43262, 0.0000000, -1.2823700],
+                   [1.43262, 0.0000000, -1.2823700]]
+
+    symbols = ['O', 'H', 'H']
+
+    normal_modes = [[[0., 0., -0.075], [-0.381, -0., 0.593], [0.381, -0., 0.593]],  # mode 1
+                    [[-0., -0., 0.044], [-0.613, -0., -0.35], [0.613, 0., -0.35]],  # mode 2
+                    [[-0.073, -0., -0.], [0.583, 0., 0.397], [0.583, 0., -0.397]]]  # mode 3
+
+    sym_modes_gs = SymmetryModes(group='c2v', coordinates=coordinates, modes=normal_modes,
+                                 symbols=symbols)
+
+    for i, mode_ref in enumerate(('A1', 'A1', 'B2')):
+        assert str(sym_modes_gs.get_state_mode(i)) == mode_ref
